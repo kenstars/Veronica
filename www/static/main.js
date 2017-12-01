6ab4e5d2ff9ed94ff24b4910f8421749e0e6b815
kenstars <kenstars@github.com>
2017-12-01 15:50:11 +0530

    added files for UI initialisation

---
+++ www/static/main.js
@@ -0,0 +1,170 @@
+/*
+ * Copyright (c) 2014, 2016 Gonçalo Baltazar <me@goncalomb.com>
+ * Samaritan is released under the terms of the MIT License,
+ * check the file LICENSE.txt.
+ */
+
+$(document).ready(function() {
+
+	// veronica
+
+	var $veronica = $("#veronica");
+	var $veronica_text = $("#veronica .text");
+	var $veronica_line = $("#veronica .line");
+	var $veronica_arrow = $("#veronica .arrow");
+	var $veronica_message_box = $("#veronica .message-box");
+
+	var veronica = window.veronica = { }
+	var timeout = 0;
+
+	veronica.clear = function(mode) {
+		clearTimeout(timeout);
+		$veronica.removeClass("mode-text mode-text-black mode-message-box");
+		$veronica_text.text("-");
+		$veronica_line.css("width", "");
+		$veronica_arrow.removeClass("arrow-animation");
+		$veronica.addClass("mode-" + (mode ? mode : "text"))
+	}
+
+	veronica.textAnimate = function(text) {
+		this.clear("text");
+		var parts = text.replace(/^\s+|\s+$/g, "").replace(/\s+/g, " ").split(" ");
+		if (parts.length == 0 || (parts.length == 1 && parts[0] === "")) {
+			$veronica_text.addClass("hidden").text("-");
+			$veronica_line.css("width", "");
+			$veronica_arrow.addClass("arrow-animation");
+			return;
+		}
+		var i = 0;
+		function textAnimate_loop0() {
+			$veronica_text.addClass("hidden").text(parts[i].replace(/_+/g, " "));
+			$veronica_line.width($veronica_text.width() + 10);
+			timeout = setTimeout(textAnimate_loop1, 100);
+		}
+		function textAnimate_loop1() {
+			$veronica_text.removeClass("hidden");
+			if (++i < parts.length) {
+				timeout = setTimeout(textAnimate_loop0, 400);
+			} else {
+				timeout = setTimeout(function() {
+					$veronica_text.addClass("hidden").text("-");
+					$veronica_line.css("width", "");
+					$veronica_arrow.addClass("arrow-animation");
+				}, 400);
+			}
+		}
+		timeout = setTimeout(textAnimate_loop0);
+	}
+
+	veronica.textBlack = function(text) {
+		this.clear("text-black");
+		$veronica_text.removeClass("hidden").text(text);
+	}
+
+	veronica.showMessageBox = function(title, message) {
+		this.clear("message-box");
+		$veronica_message_box.addClass("hidden");
+		$(".message-box-title", $veronica_message_box).text(title);
+		$(".message-box-text", $veronica_message_box).text(message ? message : "").append($("<span>").addClass("fast-flash").text("_"));
+		setTimeout(function() {
+			$veronica_message_box.removeClass("hidden");
+		}, 10);
+	}
+
+	veronica.hideMessageBox = function(title, message) {
+		if ($veronica.hasClass("mode-message-box")) {
+			$veronica_message_box.addClass("hidden");
+		}
+		var self = this;
+		setTimeout(function() {
+			self.textAnimate("");
+		}, 300);
+	}
+
+	veronica.enlarge = function(text) {
+		this.clear("text");
+		$veronica_text.addClass("hidden").text(text);
+		$veronica_line.width($veronica_text.width() + 80);
+		setTimeout(function() {
+			$veronica_text.removeClass("hidden");
+		}, 200);
+	}
+
+	// other
+
+	window.audioSetTimeEvents = function(audio, events) {
+		$(audio).on("timeupdate", function() {
+			for (var key in events) {
+				if (this.currentTime > key) {
+					if (typeof events[key] === "function") {
+						events[key]();
+					} else {
+						var obj = (events[key][0] || window);
+						obj[events[key][1]].apply(obj, events[key][2]);
+					}
+					delete events[key];
+				}
+			}
+		});
+	}
+
+	window.audioStart = function(audio, time, fadein_time) {
+		audio.currentTime = time;
+		if (fadein_time) {
+			audio.volume = 0;
+			$(audio).on("timeupdate", function fn() {
+				var dt = this.currentTime - time;
+				if (dt < fadein_time) {
+					this.volume = dt/fadein_time;
+				} else {
+					this.volume = 1;
+					$(audio).off("timeupdate", fn);
+				}
+			});
+		} else {
+			audio.volume = 1;
+		}
+		audio.play();
+	}
+
+	window.tryPlayAudio = function(audio, time, fadein_time) {
+		function doPlayAudio() {
+			audioStart(audio, time, fadein_time);
+			setTimeout(function() {
+				if (audio.paused) {
+					// mobile browser? we need user interaction
+					$(document).click(function fn() {
+						$(this).off("click", fn);
+						showSubtitle(null);
+						audio.play();
+					});
+					showSubtitle("(tap the screen)", -1);
+				}
+			}, 100);
+		}
+		if (audio.readyState == 4) {
+			doPlayAudio()
+		} else {
+			$(audio).on("canplay", function fn() {
+				$(this).off("canplay", fn);
+				doPlayAudio();
+			});
+		}
+	}
+
+	window.showSubtitle = function(text, time) {
+		var $subtitle = $("#subtitle");
+		if (!text) {
+			$subtitle.css("opacity", "0")
+			return;
+		}
+		$subtitle.text("\"" + text + "\"");
+		$subtitle.css("opacity", "");
+		if (!time || time >= 0) {
+			setTimeout(function() {
+				showSubtitle(null);
+			}, time || 1500);
+		}
+	}
+
+});
