41c2feae4302bede636624a0bf04b1fb605f2403
nikita <nikita@lightinfosys.com>
2017-12-07 19:42:51 +0530

    adding weather to web page

--- www/server.js
+++ www/server.js
@@ -6,7 +6,7 @@ var gearmanport = config.gearmanport;
 var Gearman = require("node-gearman");
 var client = new Gearman(gearmanip, gearmanport);
 var dashboard_data_creator = config.dashboard_db_reader;
-var serverPort = ui_config.serverport; 
+var serverPort = ui_config.serverport;
 
 client.connect();
 console.log("Veronica Socket server started at: ", serverPort);
@@ -17,12 +17,40 @@ function createuIdFb(data){
   }
   return data;
 }
-
+var socket_id;
 //Socket Handler
 io = require('socket.io').listen(serverPort).sockets.on('connection', function (socket)  {
+  console.log("listening...");
   socket.auth = false;
   socket.emit('initialvalue',"");
   //check the auth data sent by the client
+  socket.on('fetch_weather_details', function(data){
+    console.log("in fetch_weather_details", data);
+    socket.emit('send_append_data', data);
+  });
+
+  socket.on('send_ip_details', function(data){
+    console.log("inside server with ip data");
+    console.log(JSON.stringify(data));
+    socket_id = socket.id;
+    // var data = JSON.parse(data);
+    // data.socket_id = socket.id;
+    //   console.log("data after socket_id");
+    console.log(JSON.stringify(data));
+    job = client.submitJob("send_location_details",JSON.stringify(data));
+
+    job.on("data", function(data){
+    console.log("inside job data"+data.toString("utf-8")); // gnirts tset
+    socket.emit("send_append_data", JSON.parse(data));
+    });
+    job.on("end", function(){
+    console.log("job completed"); // gnirts tset
+    });
+    job.on("error", function(error){
+      console.log("inside error");
+    console.log(error.message); // gnirts tset
+    });
+  });
   socket.on('receive_data',function(data){
       console.log("in receive_data");
       console.log(data);
@@ -36,11 +64,11 @@ io = require('socket.io').listen(serverPort).sockets.on('connection', function (
           console.log("fb web_socket===",data.uId);
           data.session_id = data.uId;
       }
-      
+
       console.log("---" + JSON.stringify(data));
       data.timestamp = parseInt(new Date().getTime()/1000);
       job = client.submitJob("node_to_chat_tata_move",JSON.stringify(data));
-      
+
       job.on("data", function(data){
       console.log("Gearman:",data.toString("utf-8")); // gnirts tset
       //socket.emit(data.toString("utf-8"));
@@ -50,30 +78,30 @@ io = require('socket.io').listen(serverPort).sockets.on('connection', function (
       console.log('to resp_message >>> ', jsonContent);
       io.to(socket.id).emit('resp_message', jsonContent);
       });
-      
+
       job.on("end", function(){
           console.log("Job completed!");
       });
-      
+
       job.on("error", function(error){
           console.log(error.message);
       });
-      
+
   });
 
 //   ============= END CHAT FEEDBACK =============
 
     socket.on('endchatfeedback',function(data){
         console.log("\n End chat feedback===>",data);
-        
+
         });
-    
+
     socket.on('tata_move_dashboard_initialized',function(data){
         console.log("received in node", JSON.stringify(data));
         callDashboard(data, socket.id);
   });
 
-   
+
 
   function callDashboard(data, sid){
         console.log("in call Dashboard", data);
@@ -89,5 +117,5 @@ io = require('socket.io').listen(serverPort).sockets.on('connection', function (
             });
           });
     }
-    
-  });     
+
+  });
