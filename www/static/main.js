2ff06e651c4d906fc694981178834706686650fe
Pracheta <pracheta.light@gmail.com>
2017-12-07 13:20:10 +0530

    server additions

--- www/static/main.js
+++ www/static/main.js
@@ -1,8 +1,112 @@
 /*
- * Copyright (c) 2014, 2016 Gonçalo Baltazar <me@goncalomb.com>
- * Samaritan is released under the terms of the MIT License,
+ * Copyright (c) Kannan Piedy
+ * Veronica is released under the terms of the Apache License,
  * check the file LICENSE.txt.
  */
+
+
+var serverIp = '';
+var serverPort = '';
+var username = '';
+var initial_flag = 0;
+var counter = 0;
+var title_page = '';
+var title_header = '';
+var bot_id_name = "";
+var code = 212;
+var inverter = 1;
+var location_details = "";
+// var typingHtml = '<div class="row typing" style="float:right; clear:both; padding-right:20px;"><img src="image/typing.gif"></div>';
+var typingHtml = '<div class="typing" style="float:left; clear:both; padding-right:20px;"><div class="typingBot"><!--<img src="images/bot/icon1.png">--></div><div class="bubbles"><div class="bubble bubble1"></div><div class="bubble bubble2"></div><div class="bubble bubble3"></div><div class="bubble bubble4"></div></div><!--<img src="images/bot/typing.gif">--></div>';
+window.speechRecognizer = new webkitSpeechRecognition();
+
+
+$.ajax({
+    type: 'GET',
+    url: '../ui_config.json',
+    dataType: 'json',
+    success: function(config) {
+        serverIp = config.serverip;
+        serverPort = config.serverport;
+        username = config.bot_name;
+        title_page = config.title_page;
+        title_header = config.title_header;
+    },
+    async: false
+});
+var serverUrl = "http://" + serverIp + ":" + serverPort;
+var socket = io.connect(serverUrl);
+console.log(serverUrl);
+
+function sendChatVoice(text) {
+        console.log("send-chat");
+        var chatMsg = text;
+        if (chatMsg.trim() !== '') {
+			var date = new Date();
+            var timestamp = date.toString("dd MMM yyyy hh:mm:ss");
+            date = new Date(timestamp);
+            var time_stamp = date.getTime();
+            time_stamp = time_stamp /1000;
+            var dataTosSend = {'query': chatMsg, 'timestamp': time_stamp, 'session_id':socket.id, 'username':'admin', 'channel':'web', 'location_details':location_details, 'voice_flag':1};
+            console.log("data_to_send",dataTosSend);
+            socket.emit('receive_data', dataTosSend);
+        }
+    }
+
+
+function startConverting (start = 0) {
+        //var speechRecognizer = new webkitSpeechRecognition();
+        window.speechRecognizer.continuous = true;
+        window.speechRecognizer.interimResults = true;
+        window.speechRecognizer.lang = 'en';
+        console.log("starting speech");
+        if('webkitSpeechRecognition' in window && start === 1){
+            var finalTranscripts = '';
+
+            window.speechRecognizer.onresult = function(event){
+                var interimTranscripts = '';
+				//console.log(event.results);
+                for(var i = event.resultIndex; i < event.results.length; i++){
+                    var transcript = event.results[i][0].transcript;
+                    transcript.replace("\n", "<br>");
+                    if(event.results[i].isFinal){
+                        finalTranscripts += transcript;
+                    }else{
+                        interimTranscripts += transcript;
+                    }
+                }
+                //r.innerHTML = finalTranscripts + '<span style="color:#999">' + interimTranscripts + '</span>';
+                console.log("====="+finalTranscripts);
+                //document.getElementById("input_text").value = finalTranscripts;
+                if(finalTranscripts.length !== 0){
+                    sendChatVoice(finalTranscripts);
+                    console.log(finalTranscripts);
+                    finalTranscripts = '';
+                }
+            };
+            window.speechRecognizer.onspeechend = function() {
+                window.speechRecognizer.stop();
+                $("#mic_off").show();
+                $("#mic_on").hide();
+                console.log('Speech has stopped being detected');
+                
+            };
+            window.speechRecognizer.onerror = function (event) {
+                window.speechRecognizer.stop();
+                $("#mic_off").show();
+                $("#mic_on").hide();
+                console.log("error: ",event.error);
+            };
+            window.speechRecognizer.start();
+            
+        }else{
+            window.speechRecognizer.stop();
+            //r.innerHTML = 'Your browser is not supported. If google chrome, please upgrade!';
+            console.log("Either you have stopped recognition or Your browser is not supported. If google chrome, please upgrade!");
+        }
+        
+    }
+
 
 $(document).ready(function() {
 
@@ -13,7 +117,7 @@ $(document).ready(function() {
 	var $veronica_line = $("#veronica .line");
 	var $veronica_arrow = $("#veronica .arrow");
 	var $veronica_message_box = $("#veronica .message-box");
-
+	startConverting(start = 1); 
 	var veronica = window.veronica = { }
 	var timeout = 0;
 
@@ -91,7 +195,7 @@ $(document).ready(function() {
 	}
 
 	// other
-
+    
 	window.audioSetTimeEvents = function(audio, events) {
 		$(audio).on("timeupdate", function() {
 			for (var key in events) {
@@ -117,12 +221,12 @@ $(document).ready(function() {
 				if (dt < fadein_time) {
 					this.volume = dt/fadein_time;
 				} else {
-					this.volume = 1;
+					this.volume = 0;
 					$(audio).off("timeupdate", fn);
 				}
 			});
 		} else {
-			audio.volume = 1;
+			audio.volume = 0;
 		}
 		audio.play();
 	}
